1c931697e8a14d7b349c5de3f8c83d63185469ca
harry-shitt <harshithsalian335@gmail.com>
2024-06-22 09:51:28 +0530

    Initial Setup

---
+++ build/CMakeFiles/StartVFS.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StartVFS.dir/src/main.cpp.o"
+  "CMakeFiles/StartVFS.dir/src/main.cpp.o.d"
+  "StartVFS"
+  "StartVFS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StartVFS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
